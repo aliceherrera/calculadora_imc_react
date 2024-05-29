75c21d7771cfd8e112d6c7346f175394873e2df9
Alice Herrera <a.aliceherrera.com>
2024-05-29 16:30:09 -0300

    adicionando função de calcular imc

--- src/components/Formulario/index.jsx
+++ src/components/Formulario/index.jsx
@@ -1,12 +1,53 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styles from './Formulario.module.css'
 
 const Formulario = () => {
 
-    function calculaIMC() {
-        const [altura, setAltura] = useState('');
-        const [peso, setPeso] = useState('');
+    function handleSubmit(e) {
+        e.preventDefault()
     }
+
+    const [altura, setAltura] = useState(0);
+    const [peso, setPeso] = useState(0);
+
+    const renderizaIMC = () => {
+        const convertAltura = altura / 100;
+        const imc = parseFloat((peso / (convertAltura * convertAltura)).toFixed(1)); 
+
+        if (imc < 18.5) {
+            return (
+                <h2 className={styles.h2}>Seu IMC é de {imc} (Magreza)</h2>
+            )
+        }
+        else if (imc < 25) {
+            return (
+                <h2 className={styles.h2}>Seu IMC é de {imc} (Normal)</h2>
+            )
+        }
+        else if (imc < 30) {
+            return (
+                <h2 className={styles.h2}>Seu IMC é de {imc} (Sobrepeso)</h2>
+            )
+        }
+        else if (imc < 35) {
+            return (
+                <h2 className={styles.h2}>Seu IMC é de {imc} (Obesidade Grau I)</h2>
+            )
+        }
+        else if (imc < 40) {
+            return (
+                <h2 className={styles.h2}>Seu IMC é de {imc} (Obesidade Grau II)</h2>
+            )
+        }
+        else if (imc > 40) {
+            return (
+                <h2 className={styles.h2}>Seu IMC é de {imc} (Obesidade Grau III)</h2>
+            )
+        }
+        
+    }
+
+
 
     return (
         <div className={styles.card}>
@@ -18,16 +59,15 @@ const Formulario = () => {
             </div>
             <form>
                 <div className={styles.altura}>
-                    <input id='inputAltura' className={styles.input} type="number" placeholder='insira sua altura'/>
+                    <input id='inputAltura' className={styles.input} type="number" placeholder='insira sua altura' onChange={e => setAltura(e.target.value)}/>
                     <label className={styles.unidadeMedida} htmlFor="inputAltura">Altura em cm</label>
                 </div>
                 <div className={styles.peso}>
-                    <input id='inputPeso' className={styles.input} type="number" placeholder='insira seu peso'/>
+                    <input id='inputPeso' className={styles.input} type="number" placeholder='insira seu peso' onChange={e => setPeso(e.target.value)}/>
                     <label className={styles.unidadeMedida} htmlFor="inputPeso">Peso em kg</label>
                 </div>
-                <button type='submit'>Calcular o IMC</button>
+                <button type='submit' onClick={renderizaIMC}>Calcular o IMC</button>
             </form>
-            <h2 className={styles.h2}>Seu IMC é 00</h2>
         </div>
     )
 }
--- src/components/Tabela/index.jsx
+++ src/components/Tabela/index.jsx
@@ -5,34 +5,38 @@ const Tabela = () => {
         <div className="container">
             
             <table className={styles.tabela}>
-                <tr>
-                    <th className={styles.th}>IMC</th>
-                    <th className={styles.th}>Classificação</th>
-                </tr>
-                <tr>
-                    <td className={styles.td}>Menor que 18,5</td>
-                    <td className={styles.td}>Magreza</td>
-                </tr>
-                <tr>
-                    <td className={styles.td}>18,5 a 24,9</td>
-                    <td className={styles.td}>Normal</td>
-                </tr>
-                <tr>
-                    <td className={styles.td}>25 a 29,9</td>
-                    <td className={styles.td}>Sobrepeso</td>
-                </tr>
-                <tr>
-                    <td className={styles.td}>30 a 34,9</td>
-                    <td className={styles.td}>Obesidade Grau I</td>
-                </tr>
-                <tr>
-                    <td className={styles.td}>35 a 39,9</td>
-                    <td className={styles.td}>Obesidade Grau II</td>
-                </tr>
-                <tr>
-                    <td className={styles.td}>Maior que 40</td>
-                    <td className={styles.td}>Obesidade Grau III</td>
-                </tr>
+                <thead>
+                    <tr>
+                        <th className={styles.th}>IMC</th>
+                        <th className={styles.th}>Classificação</th>
+                    </tr>
+                </thead>
+                <tbody>    
+                    <tr>
+                        <td className={styles.td}>Menor que 18,5</td>
+                        <td className={styles.td}>Magreza</td>
+                    </tr>
+                    <tr>
+                        <td className={styles.td}>18,5 a 24,9</td>
+                        <td className={styles.td}>Normal</td>
+                    </tr>
+                    <tr>
+                        <td className={styles.td}>25 a 29,9</td>
+                        <td className={styles.td}>Sobrepeso</td>
+                    </tr>
+                    <tr>
+                        <td className={styles.td}>30 a 34,9</td>
+                        <td className={styles.td}>Obesidade Grau I</td>
+                    </tr>
+                    <tr>
+                        <td className={styles.td}>35 a 39,9</td>
+                        <td className={styles.td}>Obesidade Grau II</td>
+                    </tr>
+                    <tr>
+                        <td className={styles.td}>Maior que 40</td>
+                        <td className={styles.td}>Obesidade Grau III</td>
+                    </tr>
+                </tbody>
             </table>
         </div>
     )
